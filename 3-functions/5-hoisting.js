388cf88efb84d48674915b936630877791676e31
Jyasveer Gotta <jyasveer@gmail.com>
2017-09-20 11:18:07 +0530

    Class: 20-sep-2017

--- 3-functions/5-hoisting.js
+++ 3-functions/5-hoisting.js
@@ -1,28 +1,28 @@
 function run() {
     console.log('--------run----------');
 
-    console.log(test); // shold give me a error
+    console.log(test); // undefined - correct
     var test = 'hello';
-    console.log(test); // hello
+    console.log(test); // hello - correct
 
     // how does it print here when we havent create the method printHello  yet ?
-    printHello('world'); // should throw me a error
+    printHello('world'); // invalid - wrong
     function printHello(value) {
-        console.log(value); // no output
+        console.log(value); // world - correct
     }
 
-    printString('abc'); // should throw an error
+    printString('abc'); // invalid - correct ,but what is the error here ?
     var printString = function (value) {
-        console.log(value); // no putput
+        console.log(value); // we dont get anything as the above sttment is invalid
     }
 
     return 'ok';
 }
-run();
+// run();
 
 // Two take aways from this :-
 // 1. All the function definition is hoisted to the top of the scope
-// 2. All the declarations is hoisted to the top of the scope
+// 2. All the variable declarations is hoisted to the top of the scope
 
 
 // the above method will be converted to this.
@@ -45,45 +45,47 @@ function run2() {
     return 'ok';
 }
 
-//  function run3() {
-//      console.log('-----------run3-----------');
-//      var fn = function () {
-//          console.log('func 1');
-//      }
-//      fn();
-//      fn2();
-//      console.log(abc);
-//      var abc = 50;
-//      var fn = function () {
-//          console.log('func 2');
-//      }
-//      function fn2() {
-//          console.log('func 3');
-//      }
-//  }
-// //  run3();
+ function run3() {
+     console.log('-----------run3-----------');
+     var fn = function () {
+         console.log('func 1');
+     }
+     fn();
+     fn2();
+     console.log(abc);
+     var abc = 50;
+     var fn = function () {
+         console.log('func 2');
+     }
+     function fn2() {
+         console.log('func 3');
+     }
+ }
+ run3();
 
-//  // above function run3 will be converted to this functon run4
-//  function run4() {
-//      var fn = function () {
-//          console.log('func 1');
-//      }
-//      var fn;
-//      var abc;
-//      function fn2() {
-//          console.log('func 3');
-//      }
-//      fn();
-//      fn2();
-//      console.log(fn);
-//      console.log(abc);
-//      abc = 50;
-//      fn = function () {
-//          console.log('func 2');
-//      }
-//  }
+ // above function run3 will be converted to this functon run4
+ function run4() {
+     console.log("-------- run 4 ------------");
+     var fn;
+     var abc;
+     var fn;
+     function fn2() {
+         console.log('func 3');
+     }
+     fn = function () {
+         console.log('func 1');
+     }
+     fn();
+     fn2();
+     console.log(fn);
+     console.log(abc);
+     abc = 50;
+     fn = function () {
+         console.log('func 2');
+     }
+ }
 
-// //  run4();
+ run4();
 
 // // var abc = "abc";
 // // var abc;
